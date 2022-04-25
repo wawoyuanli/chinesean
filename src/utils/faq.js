efaa0c77f3ac80b3db53a5aa9f7a5116fb50bfee
wawoyuanli <1583649818@qq.com>
2022-04-25 17:11:00 +0800

    faq page

---
+++ src/utils/faq.js
@@ -0,0 +1,116 @@
+const signup = [
+  {
+    title: "What websites can become a Publisher website of ChineseAN?",
+    content:
+      "ChineseAN welcomes websites of any size, whether they are commercial or personal in nature. The Publisher needs to have the right to operate the website. As our network is based in Greater China, we require our Publisher websites not to violate the relevant laws in the following regions: Hong Kong and China (PRC). We cannot admit websites that contains obscene content.",
+  },
+  {
+    title: "Is there any fee to join the ChineseAN network?",
+    content:
+      "It is absolutely free. Signing up and using any service of the network is free. Publishers, or website owners, will receive commission but will not need to pay any fee.",
+  },
+  {
+    title:
+      "How long will it take to confirm my application for becoming a Publisher?",
+    content:
+      "You can become a Publisher right after you sign up. This does not require any approval process. You will receive a confirmation email immediately after you complete the sign-up process.",
+  },
+  {
+    title: "How long will it take to approve a website?",
+    content:
+      "ChineseAN will approve a website within 1 week and will notify the Publisher by email. The Publisher can view the latest status of the submission under the “Website Management” interface. If a website stays in “Pending” status for a much longer period, this may imply that there is no suitable ad program for this website for the moment or that ChineseAN team is trying to collect more information about the website. If you would like to speed up the approval process or provide more information to facilitate our work, please contact service@chinesean.com.",
+  },
+  {
+    title:
+      "How long will it take for an application for joining a Program to get approved?",
+    content:
+      "The time needed for the approval of such an application to a Program varies from Program to Program. First, it depends on whether the Program has opted for Automatic Approval, which means that no manual process will be involved. It will just take less than a minute to receive the confirmation. Other Programs which are set for Manual Approval need to go through the Advertiser. The time taken by different Advertisers varies from 1 day to 2 weeks. Some Advertisers may not reject websites right away during their screening process, to make open the option of accepting the website in the future. So if the status remains as “Pending” for over 2 weeks, the chance for it to get approved for a Program is relatively low but there is still a chance for it to get approved in the future.",
+  },
+];
+const joinPrograms = [
+  {
+    title: "What ad formats are supported by ChineseAN?",
+    content:
+      "ChineseAN offer a number of advanced ad display formats:T-Box with automated update and optimization functionality (Please refer to specific Q&A below);Basic ad formats (such as GIF and JPG, banners, text links, etc);Link Generator (a generator of URL which links to specific landing page of a particular product or promotion; it allows much flexibility with the format of the creative);Other resources (e.g. high-resolution images, advertorial copies);All the above tools can be downloaded from ChineseAN platform. Different Programs may have different restrictions on the means and channels of promotions. Publishers are advised to pick the appropriate means and tools according to the requirement of the particular program. For example, some Program just accept banners (including T-Box) but do not allow text links and link generator.",
+  },
+  {
+    title:
+      "How can Publishers learn about latest information of promotion programs?",
+    content:
+      "Latest information of promotion programs can be found on ChineseAN frontpage and Publisher's frontpage. As the information is updated daily, it is advised Publishers sign into ChineseAN website frequently. Also, we will use emails to send news to Publishers from time to time so that they won't miss any opportunities.",
+  },
+  {
+    title: "What is T-Box?",
+    content:
+      "T-Box is an ad code which updates banners automatically. “T” stands for “Target”, which represents the advanced targeting technology being used. Once the Publisher copy and paste the T-Box code onto the webpage, T-Box will automatically select the most suitable portfolio of programs for the traffic of the webpage; it will also automatically use the most up-to-date banners for each program. So T-Box is a tool that helps Publishers to get the best results and the most commissions, with the least effort.",
+  },
+  {
+    title: "How to set up T-Box?",
+    content:
+      "Please following the following simple steps for setting up T-Box:1. Click on the “Programs” tab, and at the “Create T-Box section”, select the size of the T-Box you need to fit into the space on your webpage.2. Fill in the basic information for the T-Box on the “Create T-Box” page. The Name of the T-Box is for reference by the Publisher only. Choosing an easy-to-remember name will help to differentiate among various T-Boxes.3. After successfully creating the T-Box, please get the code of the T-Box from the “Manage T-Box” Page. Please copy and paste the code of the T-Box you're your webpage, as in the case of normal ad codes. The setup is completed once you see a banner is being displayed at the designated ad spot on your webpage.",
+  },
+  {
+    title: "Can I use the same T-Box on more than one webpage?",
+    content:
+      "The same T-Box, or the same code, can be used on various pages under the same website domain. However, different T-Box codes must be used for different websites. Different pages of the same website may contain very different content. By using different T-Box codes for them, each will be able to display ads for programs that best fit each page. T-Box will be able to give better results.",
+  },
+  {
+    title: "Why does the T-Box on my website shows only the Ad for ChineseAN?",
+    content:
+      "An ad will only appear in a T-Box only if your website has applied to the Program and the application has been approved. The latest ad creative of that Program will be used and updated. If the T-Box on a website shows only an ad about ChineseAN, the reason is probably that the website has not successfully joined any Program or previous Programs have expired. Another reason is that the region you are in is not suitable for the Program you have applied. You can choose to skip the process of picking Programs individually by choosing to use our automated function to “join suitable programs automatically”. Please refer to separate Q&A below.",
+  },
+  {
+    title: "How should I select Programs?",
+    content:
+      "Different Programs are suitable for different websites. ChineseAN platform uses artificial intelligence to recommend programs that will likely perform well on your website based on the information you provide and on historical data. Publishers may also make use of the “Filter Options” on the “Program Search” page to classify programs by “Region”, etc.",
+  },
+  {
+    title: "How to make ad serving fully automated?",
+    content:
+      "If you want ChineseAN intelligence system to pick suitable ads for you and to save time on managing your ad, you only need to do 2 settings:1. Set your website to join Programs automatically: When you register your website, choose “Yes” under “Do you want your website to join suitable programs automatically?”. If you have already registered your website, you may still go to the “Website” interface to make the change with the option “Automatically Join Program”.2. Place a T-Box onto your website: Create a T-Box and choose “All Suitable Programs” under its “Program Options”. You only need to place the T-Box codes onto your website, ad will automatically be served. For more details on the setup of T-Box, please refer to the specific Q&A.",
+  },
+];
+const reportCommission = [
+  {
+    title: "How can I learn about my results and commissions?",
+    content:
+      "ChineseAN provides advanced reporting features. On the Publisher frontpage, you can look at a summary of every aspect of your results. They include the results and commission of the past week, commission that can be withdrawn, etc. Charts are also used to help you visualize the results. More details can be found under “Reports” in various reporting interfaces that present results according to dates, Programs, etc. In the “Event Details” interface, you can look at the exact detail of each individual event, such as transaction time, amount and referral link.",
+  },
+  {
+    title: "Why are some events not being approved?",
+    content:
+      "ChineseAN will display the tracked performance (known as “events”) in the reports. These events will need to be approved by the Advertiser before they are finally confirmed as “Approved”. For example, the approval of an event of a CPS Program depends on the payment received for the order shipment. The CPA event of a registration campaign will wait for the Advertiser to verify the quality of the registration. All the ratio of approval will be displayed on the “Approval Summary” report.",
+  },
+  {
+    title: "How can Publisher withdraw the commission earned?",
+    content:
+      "Publisher can withdraw commission, using the “Commission Application” interface under the “Commission” menu, if the balance of the account exceeds the minimum withdrawal level (the level for RMB is RMB100). We will verify the requests submitted by Thursday within the same week and release the commission according to the “Payment Method” chosen by the Publisher. The minimum withdrawal level is there to avoid paying out extraordinarily small amount which would otherwise lead to wastage of administrative charges.",
+  },
+  {
+    title:
+      "What are the payment methods and currencies supported by ChineseAN? What are the respective minimum withdrawal levels?",
+    content:
+      "We support different payment methods and currencies to suit different regions.ChineseAN platform directly calculates commission in USD, RMB and HKD. Publishers can choose to receive the commission in other local currencies: TWD, THB, JPY, SGD, AUD, MYR, IDR, PHP and INR. In such cases, USD will be used to calculate the commission balance on the platform. Payment will be made using the local currencies by converting from USD with the exchange rates at the time of commission payment.The following table lists the supported currencies, payment methods and minimum withdrawal level for different regions:",
+  },
+  {
+    title: "When can a Publisher withdraw the earned commission?",
+    content:
+      "We adopt a weekly settlement process. As long as the Publisher's account exceeds the minimum withdrawal level, he or she can request for commission withdrawal at any time. Commission requested on or before Wednesday of each week will be paid out by the weekend (Friday or Saturday). Advertisers usually confirm commission on a monthly cycle but some may take longer. They need to bring up any adjustment needed before the clearance day of each cycle.",
+  },
+  {
+    title: "Does withdrawal of commission involve any charge?",
+    content:
+      "No. Withdrawal of commission is absolutely free. ChineseAN will not impose any charge on this.",
+  },
+  {
+    title: "Do I need to pay tax for commission earned?",
+    content:
+      "ChineseAN will not pay any tax on behalf of any Publisher. We will not deduct any kind of tax from the commission earned. All Publishers will need to report and pay tax according to their domestic requirement.",
+  },
+  {
+    title:"How will the commission balance be calculated if a Program is run on a different currency?",
+    content:
+      "ChineseAN platform supports calculation of commission balance based on RMB, Hong Kong Dollar and US Dollar. If a Program is run on another currency different from the currency chosen by the Publisher to calculate the commission balance, the commission will be converted to the Publisher's currency according to the current reference foreign exchange rate before it is deposited into the ChineseAN account of the Publisher. The value of the commission will not be changed although the foreign exchange rate may fluctuate with time. The reference foreign exchange rate is fixed based on the market rate at that moment. Please refer to the foreign exchange rate table.",
+  },
+];
+export { signup, joinPrograms, reportCommission };
--- src/views/faqPage/faqPage.vue
+++ src/views/faqPage/faqPage.vue
@@ -1,10 +1,49 @@
 <template>
-    <div class="container_home">
+    <div class="container_faq">
         <header class="main-header" ref="header">
             <navc />
         </header>
         <section class="main_container" :style="{ 'margin-top': marginTop }">
-            faq
+            <div class="faq_image_bg">FAQ</div>
+            <div class="faq_box">
+                <div class="left">
+                    <p>Advertiser</p>
+                    <p>Publisher</p>
+                    <div>
+                        <p>Publisher Sign-up</p>
+                        <p>Join Programs</p>
+                        <p>Report & Commission</p>
+                    </div>
+                </div>
+                <div class="right">
+                    <p>Publisher</p>
+                    <p>Publisher Sign-up</p>
+                    <div class="el_collapse">
+                        <el-collapse v-model="activeName" accordion>
+                            <el-collapse-item v-for="(item, index) in signup" :key="index" :title="item.title">
+                                <div>{{ item.content }}</div>
+                            </el-collapse-item>
+                        </el-collapse>
+                    </div>
+                    <p>Join Programs</p>
+                    <div class="el_collapse">
+                        <el-collapse v-model="activeName" accordion>
+                            <el-collapse-item v-for="(item, index) in joinPrograms" :key="index" :title="item.title">
+                                <div>{{ item.content }}</div>
+                            </el-collapse-item>
+                        </el-collapse>
+                    </div>
+                    <p>Report & Commission</p>
+                    <div class="el_collapse">
+                        <el-collapse v-model="activeName" accordion>
+                            <el-collapse-item v-for="(item, index) in reportCommission" :key="index"
+                                :title="item.title">
+                                <div>{{ item.content }}</div>
+                            </el-collapse-item>
+                        </el-collapse>
+                    </div>
+                </div>
+            </div>
         </section>
         <footer>
             <footerC />
@@ -14,6 +53,7 @@
 <script>
 import navc from '../../components/navC.vue'
 import footerC from '../../components/footerC.vue'
+import { signup, joinPrograms, reportCommission } from '../../utils/faq.js'
 
 export default {
     name: 'home-page',
@@ -23,17 +63,24 @@ export default {
     },
     data() {
         return {
-            marginTop: 0
+            marginTop: 0,
+            activeName: '1',
+            activeNames: ['1'],
+            signup,
+            joinPrograms,
+            reportCommission
         }
     },
     mounted() {
         let header = this.$refs.header
         this.marginTop = header.clientHeight + 'px'
+
+
     }
 }
 </script>
 <style lang="scss" scoped>
-.container_home {
+.container_faq {
     .main-header {
         width: 100%;
         position: fixed;
@@ -44,7 +91,47 @@ export default {
     .main_container {
         min-height: 600px;
         text-align: center;
-        background-color: antiquewhite;
+
+
+        .faq_image_bg {
+            background-image: url('../../assets/images/faq_banner.png');
+            background-size: 100% 100%;
+            min-height: 160px;
+            background-repeat: no-repeat;
+            line-height: 160px;
+            color: #212529;
+            font-size: 30px;
+        }
+
+        .faq_box {
+            overflow: hidden;
+            width: 100%;
+            padding: 50px 0px;
+
+            .left {
+                float: left;
+                width: 40%;
+            }
+
+            .right {
+                margin-left: 40%;
+                width: 100%;
+                text-align: left;
+
+                .el_collapse {
+                    width: 900px;
+
+                    ::v-deep .el-collapse-item__header {
+                        color: #007bff;
+                        font-size: 20px;
+                    }
+
+                    ::v-deep.el-collapse-item__content {
+                        font-size: 18px;
+                    }
+                }
+            }
+        }
     }
 }
 </style>
